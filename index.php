7194c2ec7bc99d6fa647f0a7fa12c8587aeabd4a
ammarRBK <alshakmaskeain@gmail.com>
2017-11-11 21:47:25 +0200

    preparing files and run php with MAMP

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>RBK-Final-Task-PHP</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<h1>
+	Welcom to your Photo Cloud website
+			JOIN US AND SIGN UP 
+	</h1>
+	<div>
+	<center>
+	 <form method="POST" action="#">
+	 	User name: <input type="txt" name="username" placeholder="enter your username">
+	 	Password: <input type="password" name="password" placeholder="enter your password">
+	 </form>
+	 <br>
+	 <input type="submit" name="submit" value="submit signup">
+	 <br><br><br>
+	 Or if you have account you can <a href="./signin.php">Sign In</a>
+	 </center>
+	</div>
+</body>
+</html>
---
+++ signin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sign In</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div>
+	<center>
+		<br><br><br>
+	 <form method="POST" action="#">
+	 	User name: <input type="txt" name="username" placeholder="enter your username">
+	 	Password: <input type="password" name="password" placeholder="enter your password">
+	    <input type="submit" name="submit" value="Submit">
+	 </form>
+	 <br>
+	 </center>
+	</div>
+</body>
+</html>
