6ddf102e3d0295136232a4e3932ccd2eb843ee38
ammarRBK <alshakmaskeain@gmail.com>
2017-11-13 16:28:46 +0200

    forward with connecting to database

--- connect-mysql-mariadb.php
+++ connect-mysql-mariadb.php
@@ -1,15 +1,24 @@
   <!-- connot connect to database -->
+ <?php
+  // define (DB_USER, "root");
+  // define (DB_PASSWORD, "gOJTkc7QSL");
+  // define (DB_DATABASE, "rbkLastTask");
+  // define (DB_HOST, "https://node42861-env-2276018.mircloud.host");
+?> -->
   <?php  
-   $server = "https://node42861-env-2276018.mircloud.host";
-  $user = "root";
-  $pw = "gOJTkc7QSL";
-  $db = "rbkLastTask";
-   $connect = mysql_connect($server,$user,$pw,$db);
-   if(!$connect){
-    die("connot connect to database $db on server $server using
-       username $user (".mysql_errno().", ".mysql_connect_error().")");
-    echo "connot connect";
-   }else{
-   echo "you have connection to your database";
- }
+  // $server = "node42861-env-2276018.mircloud.host";
+  // $user = "root";
+  // $pw = "DHIgiv98012";
+  // $db = "rbkLastTask";
+  // $con = mysqli_connect($server,$user,$pw,$db);
+  $con=mysqli_connect("node42861-env-2276018.mircloud.host","root","DHIgiv98012","rbkLastTask");
+  Echo mysqli_connect_error();
+  if($con)
+  {
+      echo "success";
+  }
+  else
+  {
+      echo "failed";
+  }
   ?>  
